3eeca052aa2a077012ead730ab8755887f3a5699
dat <levandatz2021@gmail.com>
2023-02-21 15:55:17 +0700

    taiweblan1

---
+++ tinhpsterst.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e,f;
+	printf("\nhap phan tu cua phan so thu nhat :");
+	scanf("%d", &a);
+	printf("\nnhap phan mau cua phan so thu nhat :");
+	scanf("%d", &b);
+}
